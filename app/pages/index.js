2d817b101f1ba1649d61cf85386e5ed5c02e9ee8
shauncoree <shauncoree@gmail.com>
2022-10-18 19:02:37 +0100

    CM / sugar implementation

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,24 +1,45 @@
 import React from "react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const Home = () => {
-    return (
-        <div className="App">
-            <div className="container">
-                <div className="header-container">
-                    <p className="header">🍭 Candy Drop</p>
-                    <p className="sub-text">NFT drop machine with fair mint</p>
-                </div>
-                <div className="footer-container">
-                    <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-                    <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`built on @${TWITTER_HANDLE}`}</a>
-                </div>
-            </div>
+  const wallet = useWallet();
+
+  const renderNotConnectedConatainer = () => (
+    <div>
+      <img
+        src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif"
+        alt="emojji"
+      />
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">🍭 Candy Drop</p>
+          <p className="sub-text">NFT drop machine with fair mint</p>
+          {wallet.publicKey ? "Hello" : renderNotConnectedConatainer()}
         </div>
-    );
+        <div className="footer-container">
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
