016ca04b0b8fb7d17b9be692f97ff61d01fa6649
shauncoree <shauncoree@gmail.com>
2022-10-18 21:09:41 +0100

    Mint away!

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import CandyMachine from "../components/CandyMachine";
 import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
@@ -9,7 +10,7 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const Home = () => {
   const wallet = useWallet();
 
-  const renderNotConnectedConatainer = () => (
+  const renderNotConnectedContainer = () => (
     <div>
       <img
         src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif"
@@ -23,23 +24,20 @@ const Home = () => {
 
   return (
     <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header">🍭 Candy Drop</p>
-          <p className="sub-text">NFT drop machine with fair mint</p>
-          {wallet.publicKey ? "Hello" : renderNotConnectedConatainer()}
+        <div className="container">
+            <div className="header-container">
+                <p className="header">🍭 Candy Drop</p>
+                <p className="sub-text">NFT drop machine with fair mint</p>
+                {/* Render your connect to wallet button right here */}
+                {wallet.publicKey ? <CandyMachine walletAddress={wallet} /> : renderNotConnectedContainer()}
+            </div>
+            <div className="footer-container">
+                <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+                <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`built on @${TWITTER_HANDLE}`}</a>
+            </div>
         </div>
-        <div className="footer-container">
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-        </div>
-      </div>
     </div>
-  );
+);
 };
 
 export default Home;
